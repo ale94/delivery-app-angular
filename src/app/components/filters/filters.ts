bef2ae080125a9b8cc0eabdd577d5a1501cea9bc
ale94 <alejandrorua111@gmail.com>
2025-10-25 17:54:13 -0300

    refactor: refactor filter component

--- src/app/components/filters/filters.ts
+++ src/app/components/filters/filters.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'filters',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './filters.html'
 })
 export class Filters {
