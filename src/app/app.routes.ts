fd20758b6b5112fc3124f42901d81dd535278f5a
ale94 <alejandrorua111@gmail.com>
2025-10-25 17:51:49 -0300

    refactor: refactor routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page';
+import { SearchPage } from './pages/search-page/search-page';
 
 export const routes: Routes = [
   {
     path: "home",
     component: HomePage
+  },
+  {
+    path: "search/:food",
+    component: SearchPage
   }
+
 ];
