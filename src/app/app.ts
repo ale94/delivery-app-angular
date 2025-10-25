c4fa4129bae631672ea55ef4fb928c4d7ced4799
ale94 <alejandrorua111@gmail.com>
2025-10-25 17:55:22 -0300

    refactor: refactor app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Topbar } from "./components/topbar/topbar";
+import { SiteFooter } from "./components/site-footer/site-footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Topbar, SiteFooter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
