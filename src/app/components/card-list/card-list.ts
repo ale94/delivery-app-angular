dfbafff0f96620ae33382452a86eea09dca6fd12
ale94 <alejandrorua111@gmail.com>
2025-10-25 17:53:35 -0300

    refactor: refactor card list, hero and hore component

--- src/app/components/card-list/card-list.ts
+++ src/app/components/card-list/card-list.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CardItem } from "../card-item/card-item";
+import { Shops } from '../../services/shops';
 
 @Component({
   selector: 'card-list',
-  imports: [],
+  imports: [CardItem],
   templateUrl: './card-list.html'
 })
 export class CardList {
+
+  shopService = inject(Shops);
 
 }
--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { Topbar } from "../../components/topbar/topbar";
 import { Hero } from "../../components/hero/hero";
 import { Filters } from "../../components/filters/filters";
 import { CardList } from "../../components/card-list/card-list";
-import { SiteFooter } from "../../components/site-footer/site-footer";
 
 @Component({
   selector: 'app-home-page',
-  imports: [Topbar, Hero, Filters, CardList, SiteFooter],
+  imports: [Hero, Filters, CardList,],
   templateUrl: './home-page.html'
 })
 export class HomePage {
